0572c3be5cc196b707853fd6d8d5f6af6e35b314
Nick <nicholasee97@gmail.com>
2022-10-15 01:54:22 +0800

    Create getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello, World! Test")
+// getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play.
+
+function getComputerChoice() {
+    const arr = ['Rock','Paper','Scissors'];
+    return arr[Math.floor(Math.random()*arr.length)];
+    
+}
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+
+/* Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation)
+*/
